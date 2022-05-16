565a256f2ab4ceb2340a335005a182321a5a1c07
Isabell Hammer <isabellhammer@yahoo.de>
2022-05-16 15:51:48 +0200

    questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,12 @@ class QuestionsController < ApplicationController
 
     def answer
       @answer = params[:answer]
+      if @answer.downcase == "i am going to work" then
+        @result = "Great!"
+      elsif @answer.include?("?") then
+        @result = "Silly question, get dressed and go to work!"
+      else
+        @result = "I don't care, get dressed and go to work!"
+      end
     end
 end
